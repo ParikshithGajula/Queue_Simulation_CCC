ebccad0f66fbc2d3eef9ac4b8dc15044445168a1
ParikshithGajula <parikshithgajula@gmail.com>
2025-12-09 16:51:28 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/queue-tickets/",
   server: {
     host: "0.0.0.0",
     port: 8080,
